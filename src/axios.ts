b1f0f9d1e3f4ce0c6118b1ebc6cd8d54ef54d78a
Dan <jazurduymontalvo@gmail.com>
2024-02-24 18:12:03 +0100

    Home Marker Style

--- src/axios.ts
+++ src/axios.ts
@@ -2,3 +2,8 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/'
 // axios.defaults.baseURL = 'http://192.168.1.75/'
+
+// Set the provided JWT token in the headers
+//axios.defaults.headers['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4NTM0Njg2LCJpYXQiOjE3MDg1MzQzODYsImp0aSI6IjlhYzA0MzEzODcyZDRkNDhiMGI5MjQyYzVlNjNjMjJhIiwidXNlcl9pZCI6Mn0.hDziwMLYMEs_3nXW8JKFNS189LDCXCmQoaGWx9cO5-8';
+//axios.defaults.headers['x-api-key'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4NTM0Njg2LCJpYXQiOjE3MDg1MzQzODYsImp0aSI6IjlhYzA0MzEzODcyZDRkNDhiMGI5MjQyYzVlNjNjMjJhIiwidXNlcl9pZCI6Mn0.hDziwMLYMEs_3nXW8JKFNS189LDCXCmQoaGWx9cO5-8';
+export default axios;
--- src/components/HomeMap.vue
+++ src/components/HomeMap.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="flex flex-row w-full">
     <div id="mapContainer" class="w-[1000px] h-screen" />
-    <!-- <input type="radio" name="gender" value="male" /> Satelite
-    <input type="radio" name="gender" value="female" /> Female -->
     <div class="w-1/3 h-full bg-gray-500">
-      <form class="w-[100px] absolute top-4 left-20 z-[1000] flex flex-col sm:flex-col md:flex-col">
+      <form
+        @submit.prevent="startJourney"
+        class="w-[100px] absolute top-4 left-20 z-[1000] flex flex-col sm:flex-col md:flex-col"
+      >
         <div>
           <select
             v-for="student in students_by_teacher"
@@ -14,23 +15,27 @@
             <option>{{ student.name }}</option>
           </select>
         </div>
-
-        <div>
-          <button
-            type="submit"
-            @click="startJourney"
-            class="rounded-md px-2 py-1 mx-1 my-1 bg-green font-semibold"
-          >
-            Iniciar
-          </button>
-          <button
-            v-if="showPauseBotton"
-            class="rounded-md px-2 py-1 mx-1 my-1 bg-orange-500 font-semibold"
-          >
+        <button
+          type="submit"
+          @click="startJourney"
+          id="startJourney"
+          class="w-10 h-10 rounded-md px-2 py-1 mx-1 my-1 bg-green font-semibold"
+        >
+          Iniciar
+        </button>
+        <div v-if="showPauseButton">
+          <button @click="stopJourney">Detener</button>
+          <button class="rounded-md px-2 py-1 mx-1 my-1 bg-orange-500 font-semibold">
             <font-awesome-icon icon="pause" />
           </button>
         </div>
       </form>
+      <button
+        class="rounded-md px-2 py-1 mx-1 my-1 bg-orange-500 font-semibold"
+        @click="stopJourney"
+      >
+        Stop
+      </button>
       <details class="rounded-md shadow-md p-4" v-for="type_foul in type_fouls" :key="type_foul.id">
         <summary
           class="cursor-pointer bg-gray-200 hover:bg-gray-300 py-2 px-4 rounded-md outline-none"
@@ -54,7 +59,7 @@
 </template>
 
 <script>
-import { defineComponent, onMounted, onBeforeMount } from 'vue'
+import { defineComponent, onMounted, onBeforeMount, ref } from 'vue';
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import router from '@/router'
@@ -80,8 +85,8 @@ export default defineComponent({
         faults: []
       },
 
-      // Show pause botton
-      showPauseBotton: false
+      // Show pause button
+      showPauseButton: false
     }
   },
   mounted() {
@@ -122,13 +127,6 @@ export default defineComponent({
         .catch((error) => {
           console.log(error)
         })
-    },
-
-    // Show pause botton
-    startJourney() {
-      // 
-      this.showPauseBotton = true
-
     }
   },
   name: 'LMap',
@@ -141,6 +139,10 @@ export default defineComponent({
   },
   setup(props) {
     let map = null
+    let route = []
+    let routerLayer
+    let showPauseButton = ref(false);
+    let watchId
 
     onMounted(() => {
       createMapLayer()
@@ -156,63 +158,114 @@ export default defineComponent({
       map = L.map('mapContainer', {
         center: [0, 0],
         zoom: 13
-      }).setView([0, 0], 22)
+      }).setView([0, 0], 22);
+      let userMarker;
+      let userMarkerAround;
 
       try {
         // Create a marker for the user's current position
-        const userMarker = L.circle([0, 0], 2.5).addTo(map)
-        // Postion marker style
-        userMarker.style = {
-          color: 'blue',
+        userMarker = L.circle([0, 0], 2.5, {
+          color: 'white',
           fillColor: 'blue',
-          fillOpacity: 0.5,
-          radius: 0.2
-        }
+          fillOpacity: 1,
+          opacity: 1
+        }).addTo(map)
+
+        userMarkerAround = L.circle([0, 0], 20, {
+          weight: 0.5,
+          color: '#8085FF',
+          fillColor: '#8085FF',
+          fillOpacity: 0.2,
+          lineCap: 'round'
+        }).addTo(map)
         // Start tracking the user's position
         watchId = navigator.geolocation.watchPosition(
           (position) => {
             const { latitude, longitude } = position.coords
-
             // Update the marker's position
             userMarker.setLatLng([latitude, longitude])
+            userMarkerAround.setLatLng([latitude, longitude])
 
             // Center the map on the marker
             map.panTo([latitude, longitude], { animate: true, duration: 1 })
           },
           (error) => {
-            displayError(`Error during tracking: ${error.message}`)
+            console.log(`Error during tracking: ${error.message}`)
           }
         )
       } catch (error) {
         console.log(`Error starting tracking: ${error.message}`)
       }
 
+      // Satellite layer
       const satelliteLayer = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
         maxZoom: 22,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
       })
-
-      const openTopoMap = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> Danjuaz'
-      }).addTo(map)
-
-      const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      // Add base layers to the map
+      const streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      })
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(map) // Add streets layer by default
 
       const baseLayers = {
-        Satelite: satelliteLayer
+        Relieve: streets,
+        Satellite: satelliteLayer
       }
-      const layerControl = L.control.layers(baseLayers).addTo(map)
-      layerControl.addBaseLayer(openTopoMap, 'Relieve')
+      // Add layer control to the map
+      L.control.layers(baseLayers).addTo(map);
+      
+      return { map, userMarker, userMarkerAround }
     }
 
-    const setMarkers = () => {
-      props.markers.map((marker) => {
-        return L.marker([marker.latitude, marker.longitude]).addTo(map).bindPopup(marker.descricao)
-      })
+    // Show pause botton
+    const startJourney = ({ map, userMarker, userMarkerAround }) => {
+      // Verificar si el navegador es compatible con la API Screen Wake Lock
+      if ('wakeLock' in navigator) {
+        // Solicitar el bloqueo de pantalla activo
+        navigator.wakeLock
+          .request('screen')
+          .then((wakeLockObj) => {
+            console.log('Bloqueo de pantalla activado')
+
+            showPauseButton.value = true
+
+            try {
+              watchId = navigator.geolocation.watchPosition(
+                (position) => {
+                  const { latitude, longitude } = position.coords
+                  route.push([latitude, longitude])
+
+                  // Update the marker position
+                  userMarker.setLatLng([latitude, longitude])
+                  userMarketAround.setLatLng([latitude, longitude])
+
+                  // Center the map on the marker
+                  map.panTo([latitude, longitude])
+
+                  drawRoute()
+                },
+                (error) => {
+                  console.log(`Error durante el seguimiento: ${error.message}`)
+                }
+              )
+            } catch (error) {
+              console.log(`Error al iniciar el seguimiento: ${error.message}`)
+            }
+
+            // Agregar un manejador de eventos para el caso de que el bloqueo se suelte por alguna razón
+            wakeLockObj.addEventListener('release', () => {
+              console.log('El bloqueo de pantalla se ha soltado')
+            })
+          })
+          .catch((error) => {
+            console.error('Error al solicitar el bloqueo de pantalla: ', error)
+          })
+      } else {
+        console.warn('La API de bloqueo de pantalla no está disponible en este navegador.')
+      }
     }
+
     const drawRoute = () => {
       if (route.length > 1) {
         if (!routeLayer) {
@@ -226,10 +279,27 @@ export default defineComponent({
         weight: 5,
         opacity: 0.8
       })
-      const markerInitial = L.marker(route[0]).addTo(map).bindPopup('I am a marker.')
-      const markerFinal = L.marker(route[route.length - 1])
-        .addTo(map)
-        .bindPopup('I am a marker.')
+    }
+
+    const stopJourney = () => {
+      console.log('Detener')
+      try {
+        // Detener el seguimiento continuo
+        navigator.geolocation.clearWatch(watchId)
+
+        route = []
+
+        if (routeLayer) {
+          map.removeLayer(routeLayer)
+          routeLayer = undefined
+        }
+      } catch (error) {
+        console.log(`Error al detener el seguimiento: ${error.message}`)
+      }
+    }
+    return {
+      startJourney,
+      stopJourney
     }
   }
 })
