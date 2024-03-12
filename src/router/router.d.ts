0a927d29255bfb5fb89d5e9687cecf61620f5996
Dan <jazurduymontalvo@gmail.com>
2024-03-12 13:55:28 +0100

    Update config can build

---
+++ src/router/router.d.ts
@@ -0,0 +1,53 @@
+// router.d.ts
+declare module '@views/LoginView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+declare module '@views/HomeView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+declare module '@views/StudentsView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+declare module '@views/StudentView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+declare module '@views/CreateStudentView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+  
+declare module '@views/CreateTownView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+declare module '@views/LessonsView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+declare module '@views/CreateFoulView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+declare module '@views/CreateTypeFoulView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+declare module '@views/LessonView.vue' {
+    import { Component } from 'vue';
+    const component: Component;
+    export default component;
+};
+  
